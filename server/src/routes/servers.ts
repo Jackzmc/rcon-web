8a390965cdb90401ecf5c8b9eb154b189e951ebf
Jackz <me@jackz.me>
2021-07-13 13:09:39 -0500

    Implement /api/servers stubs

--- server/src/routes/servers.ts
+++ server/src/routes/servers.ts
@@ -1,16 +1,29 @@
 import Database  from '../database.js'
 import { Router } from 'express'
+import { ErrorCode } from '../util';
+import { Express } from 'express'
 
 const router = Router()
 
-export default function(db: Database) {
+export default function(app: Express, db: Database) {
   router.get('/:id', async(req, res) => {
-
+    const result = await db.Servers.findOne(req.params.id)
+    if(result) res.json({
+      ...result,
+      players: [],
+      status: true
+    })
+    else res.status(404).json(app.locals.error(ErrorCode.SERVER_NOT_FOUND))
   })
   router.get('/', async(req, res) => {
-    const servers = await db.Servers.find()
-    console.log(servers)
-    res.json(servers)
+    const result = await db.Servers.find()
+    res.json(result.map(server => {
+      return {
+        ...server,
+        players: [],
+        status: true
+      }
+    }))
   })
   return router
 }
